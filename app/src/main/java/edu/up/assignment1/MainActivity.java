4ab52a02df9fd596ce47038e61b3b48c770f992d
Shubu Aryal <scaryal06@gmail.com>
2024-02-15 20:11:01 -0800

    hw part a

---
+++ app/src/main/java/edu/up/assignment1/MainActivity.java
@@ -0,0 +1,16 @@
+// @author Shubu Aryal
+
+package edu.up.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
